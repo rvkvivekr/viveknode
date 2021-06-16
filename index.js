ba7d149d7372a3f839e136b8bb34dea42b692e2f
rvkvivekr <85954214+rvkvivekr@users.noreply.github.com>
2021-06-16 12:08:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/Plans/estimate', function(req, res) {
+app.get('/', function(req, res) {
     res.json(data);   
 });
 
